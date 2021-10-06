0b03c77c7bf9c71a1f5892ac5177f17a55b19074
TaraStanko <91203149+TaraStanko@users.noreply.github.com>
2021-10-06 23:18:10 +0300

    Add files via upload

---
+++ алгоритм 4.1.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k,n ;
+	double S;
+	cout << "n = "; cin >> n;
+	cout << "k = "; cin >> k;
+	
+	S = 0;
+	k = n;
+	while (k <= 25)
+	{
+		S *= sqrt(1+(n /1.*k));
+		k++;
+
+	}
+	cout << S << endl;
+	//jjjjjjjjjjjjjjjjjjjjjjjjj
+		S = 0;
+		k = n;
+	do {
+		S *= sqrt(1 + (n / 1. * k));
+		k++;
+	} while (k <= 25);
+	cout << S << endl;
+	//ddddddddddddddddddddddddo
+	S = 0;
+	for (k = n; k <= 25;k++)
+	{
+		S *= sqrt(1 + (n / 1. * k));
+	}
+	cout << S << endl;
+	//iiiiiiiiiiiiiiiiiiiiiiiii
+	S = 0;
+	for (k = n; k >= 25; k--)
+	{
+		S *= sqrt(1 + (n / 1. * k));
+	}
+	cout << S << endl;
+	return 0;
+}
